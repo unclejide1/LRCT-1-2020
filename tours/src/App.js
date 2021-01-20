9b04c4a3188745d6acf1ea240fd4873703478277
Jideofor <njusticej@gmail.com>
2021-01-20 21:01:45 +0100

    feat: hadle case of no tour left

--- tours/src/App.js
+++ tours/src/App.js
@@ -39,6 +39,15 @@ function App() {
       </main>
     )
   }
+  if(tours.length === 0){
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour}/>
