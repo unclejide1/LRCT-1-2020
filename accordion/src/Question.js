f2306f4c69f6f414ab14c3ecb212c35ab06cb4b0
Jideofor <njusticej@gmail.com>
2021-01-22 01:38:07 +0100

    feat: add single questions page and functionality

--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+  const [showAnswer, setShowAnswer] = useState(false)
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowAnswer(!showAnswer)}>
+          {showAnswer ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {showAnswer && <p>{info}</p>}
+    </article>
+  )
 };
 
 export default Question;
