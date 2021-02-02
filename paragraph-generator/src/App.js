8d22c44d74ed20998bc2878b551ec78bc2ec29d2
Jideofor <njusticej@gmail.com>
2021-02-02 17:21:52 +0100

    feat: build the generator component

--- paragraph-generator/src/App.js
+++ paragraph-generator/src/App.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
 function App() {
-  
-  
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data)
+  }
+  console.log(data.length)
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h2>tired of boring lorem ipsum</h2>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+        
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
