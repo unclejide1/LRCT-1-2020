3860e66e35f62f595187eb1ab57316c805ec58f8
Jideofor <njusticej@gmail.com>
2021-02-04 00:12:14 +0100

    refactor: move useEffect to parent component

--- grocery-bud/src/Alert.js
+++ grocery-bud/src/Alert.js
@@ -1,12 +1,7 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({type, msg, removeAlert}) => {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      removeAlert()
-    }, 3000)
-    return () => clearTimeout(timeout)
-  }, [])
+const Alert = ({type, msg, removeAlert, list}) => {
+  
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
