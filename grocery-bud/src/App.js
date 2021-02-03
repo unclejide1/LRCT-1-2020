ae05c7140e614f41d6465ff44f36067fd14ded80
Jideofor <njusticej@gmail.com>
2021-02-03 22:41:06 +0100

    feat: add form to collect grocery list items

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -8,11 +8,39 @@ function App() {
   const [isEdit, setEdit] = useState(false)
   const [editId, setEditId] = useState(null)
   const [alert, setAlert] = useState({show: false, msg: '', type:''})
-  return <section className ="section-center">
-    <div className="grocery-container">
-      
-    </div>
-  </section>
+
+   const handleSubmit = (e) => {
+     e.preventDefault()
+    console.log('submitted')
+    console.log(name)
+    
+   }
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            value={name}
+            placeholder='e.g eggs'
+            onChange={(e) => setName(e.target.value)}
+            className="grocery"
+          />
+          <button type='submit' className='submit-btn'>
+            {isEdit ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
