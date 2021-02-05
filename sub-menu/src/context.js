33f92d98aafb63a5ebc6ccc47e7d0e8f5bb540e2
Jideofor <njusticej@gmail.com>
2021-02-05 18:41:52 +0100

    feat: set up context

--- sub-menu/src/context.js
+++ sub-menu/src/context.js
@@ -1,2 +1,45 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true)
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true)
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true)
+  }
+  const closeSideBar = () => {
+    setIsSideBarOpen(false)
+  }
+
+  const openSubMenu = () => {
+    setIsSubMenuOpen(true)
+  }
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false)
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        isSubMenuOpen,
+        isSideBarOpen,
+        openSubMenu,
+        openSideBar,
+        closeSubMenu,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+//customhook
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
