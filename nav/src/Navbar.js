6372702ddad0133b939211f16a63a40971b6f2e4
Jideofor <njusticej@gmail.com>
2021-02-05 02:42:09 +0100

    refactor: remove hardcoded routes and text

--- nav/src/Navbar.js
+++ nav/src/Navbar.js
@@ -15,19 +15,15 @@ const Navbar = () => {
         </div>
         <div className='links-container show-container'>
           <ul className='links'>
-            <li>
-              <a href='#'>home</a>
-            </li>
-
-            <li>
-              <a href='#'>about</a>
-            </li>
-            <li>
-              <a href='#'>contact</a>
-            </li>
-            <li>
-              <a href='#'>product</a>
-            </li>
+            {links.map((link) => {
+              const {id, url, text} = link
+              return (
+                <li key ={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+      
           </ul>
         </div>
         <ul className='social-icons'>
