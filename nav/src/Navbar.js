14c7b1a4ce79679befa043a7c0754118bff0f3cc
Jideofor <njusticej@gmail.com>
2021-02-05 02:44:48 +0100

    refactor: remove hardcoded social links

--- nav/src/Navbar.js
+++ nav/src/Navbar.js
@@ -16,37 +16,24 @@ const Navbar = () => {
         <div className='links-container show-container'>
           <ul className='links'>
             {links.map((link) => {
-              const {id, url, text} = link
+              const { id, url, text } = link
               return (
-                <li key ={id}>
+                <li key={id}>
                   <a href={url}>{text}</a>
                 </li>
               )
             })}
-      
           </ul>
         </div>
         <ul className='social-icons'>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
+          {social.map((link) => {
+            const { id, url, icon } = link
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
