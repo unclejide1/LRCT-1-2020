4e2a41b40738d7d9be908a39a6df3f6fe456f1fb
Jideofor <njusticej@gmail.com>
2021-02-05 02:37:12 +0100

    feat: set up skeletal structure for links

--- nav/src/Navbar.js
+++ nav/src/Navbar.js
@@ -4,7 +4,56 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <butoon className='nav-toggle'>
+            <FaBars />
+          </butoon>
+        </div>
+        <div className='links-container show-container'>
+          <ul className='links'>
+            <li>
+              <a href='#'>home</a>
+            </li>
 
+            <li>
+              <a href='#'>about</a>
+            </li>
+            <li>
+              <a href='#'>contact</a>
+            </li>
+            <li>
+              <a href='#'>product</a>
+            </li>
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          <li>
+            <a href='https://www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
 export default Navbar
