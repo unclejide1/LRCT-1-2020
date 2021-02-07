07640756f91830583f99f2bcfb04e5e3b73f5de9
Jideofor <njusticej@gmail.com>
2021-02-08 00:02:45 +0100

    feat: add remove item functionality

--- cart/src/CartContainer.js
+++ cart/src/CartContainer.js
@@ -37,7 +37,7 @@ const CartContainer = () => {
         </div>
         <button
           className='btn clear-btn'
-          onClick={() => clearCart()}
+          onClick={clearCart}
         >
           clear cart
         </button>
--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -2,6 +2,10 @@ const reducer =(state, action) => {
  if(action.type === 'CLEAR_CART'){
   return {...state, cart:[]}
  }
+ if (action.type === 'REMOVE') {
+  const newCart = state.cart.filter((item) => item.id !== action.payload)
+   return { ...state, cart: newCart }
+ }
  return state
 }
 
