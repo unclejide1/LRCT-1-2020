809662a9e3953b453aafd9dcbb3e4ff64247522c
Jideofor <njusticej@gmail.com>
2021-02-08 00:20:04 +0100

    feat: add increase and decrease amount funtionality

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -6,6 +6,25 @@ const reducer =(state, action) => {
   const newCart = state.cart.filter((item) => item.id !== action.payload)
    return { ...state, cart: newCart }
  }
+
+ if (action.type === 'INCREASE') {
+   let tempCart = state.cart.map((item) => {
+    if (item.id === action.payload){
+     return {...item, amount: item.amount + 1}
+    }
+   return item
+  })
+   return { ...state, cart: tempCart }
+ }
+ if (action.type === 'DECREASE') {
+   let tempCart = state.cart.map((item) => {
+     if (item.id === action.payload && (item.amount>0)) {
+       return { ...item, amount: item.amount - 1 }
+     }
+     return item
+   }).filter((item) => item.amount >= 1)
+   return { ...state, cart: tempCart }
+ }
  return state
 }
 
