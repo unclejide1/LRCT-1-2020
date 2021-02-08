595f22ed773d37fbfb46c63a821e17bb476a0c0e
Jideofor <njusticej@gmail.com>
2021-02-08 17:05:07 +0100

    feat: add error page

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus facilis quo, autem temporibus ut rerum libero? Eos similique ducimus sint animi quis impedit libero ullam ut cupiditate magni. Suscipit, minima!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nam corporis in nemo quae blanditiis. Amet voluptates atque, ipsam rem, ratione, explicabo totam accusantium ab enim dolore necessitatibus? Quae, sequi!
+      </p>
+    </section>
   )
 }
 
