6db2bbdf41b3b77a105f157af3974f91d04307c2
Jideofor <njusticej@gmail.com>
2021-02-08 13:08:51 +0100

    feat: connect to external API

--- cart/src/context.js
+++ cart/src/context.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
 import reducer from './reducer'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
@@ -8,7 +7,7 @@ const AppContext = React.createContext()
 
 const initialState = {
   loading: false,
-  cart: cartItems,
+  cart: [],
   total: 0,
   amount: 0
 }
@@ -35,6 +34,16 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: 'DECREASE', payload: id })
   }
+
+  const fetchData = async() => {
+    dispatch({type: 'LOADING'})
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload:cart })
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <AppContext.Provider
