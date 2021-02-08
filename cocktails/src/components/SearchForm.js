f99a923c891c60e9dcb3f8a843a6f10914ee5c8a
Jideofor <njusticej@gmail.com>
2021-02-08 18:46:38 +0100

    feat: add search cocktails functionality

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext()
+  const searchValue = useRef('')
+  useEffect(() => {
+    searchValue.current.focus()
+  })
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form  className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input type="text" id='name' ref={searchValue} onChange={searchCocktail} onSubmit={handleSubmit}/>
+        </div>
+      </form>
+    </section>
   )
 }
 
