406f00e5f7fa3926a9a4598652b2f5f9400a1058
Jideofor <njusticej@gmail.com>
2021-02-11 20:50:48 +0100

    feat: add functionality to fetch from api

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -6,6 +6,26 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+  const fetchImages = async() => {
+    setLoading(true)
+    let url;
+    url = `${mainUrl}?cient_id=`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
   return <h2>stock photos starter</h2>
 }
 
