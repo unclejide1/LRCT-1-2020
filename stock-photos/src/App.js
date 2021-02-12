05486e60a10ae487439b1354a93e46c0ba078757
Jideofor <njusticej@gmail.com>
2021-02-12 12:23:45 +0100

    feat: add infinite scroll feature

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -8,14 +8,20 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
   const fetchImages = async() => {
     setLoading(true)
     let url;
-    url = `${mainUrl}${clientID}`
+    const urlPage = `&page=${page}`
+    url = `${mainUrl}${clientID}${urlPage}`
+    console.log(url);
+    console.log(page);
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setPhotos(data)
+      setPhotos((oldPhotos) => {
+        return [...oldPhotos, ...data]
+      })
       console.log(data)
       setLoading(false)
     } catch (error) {
@@ -26,12 +32,14 @@ function App() {
 
   useEffect(() => {
     fetchImages()
-  }, [])
+  }, [page])
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
       if(!loading && (window.innerHeight + window.scrollY) >= (document.body.scrollHeight -2 )){
-        console.log("it worked")
+        setPage((oldPage) => {
+          return oldPage + 1
+        })
       }
       
     })
@@ -46,6 +54,8 @@ function App() {
   const handleChange = () => {
 
   }
+
+  
   return <main>
     <section className="search">
       <form className="search-form">
