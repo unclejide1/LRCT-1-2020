1727cc39f33aa847dcbcdd1b27e2e58c0cab4f92
Jideofor <njusticej@gmail.com>
2021-02-12 10:24:45 +0100

    feat: pass access key through .env

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
@@ -11,7 +11,7 @@ function App() {
   const fetchImages = async() => {
     setLoading(true)
     let url;
-    url = `${mainUrl}?cient_id=`
+    url = `${mainUrl}${clientID}`
     try {
       const response = await fetch(url)
       const data = await response.json()
