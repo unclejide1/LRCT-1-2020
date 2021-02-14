b7cb05a2894ddb53c1c3d5dad3da33c65220d8ed
Jideofor <njusticej@gmail.com>
2021-02-14 22:03:40 +0100

    feat: add search functionality

--- movie-db/src/SearchForm.js
+++ movie-db/src/SearchForm.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const SearchForm = () => {
-  return <h2>search component</h2>
+  const {query, setQuery, error} = useGlobalContext();
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+  return <form className="search-form"  onSubmit={(e) => e.preventDefault()}>
+    <h2>search movies</h2>
+    <input type="text" value={query} onChange={handleChange} className="form-input"/>
+    {error.show && <div className='error'>{error.msg}</div>}
+  </form>
 }
 
 export default SearchForm
