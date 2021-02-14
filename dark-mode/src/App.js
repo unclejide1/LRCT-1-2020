ebced93d18e0bb7a4818cd807e5fcd8ad37871e2
Jideofor <njusticej@gmail.com>
2021-02-14 02:15:01 +0100

    feat: add them toggle functionality

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -3,11 +3,24 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  useEffect(()=>{
+    document.documentElement.className = theme
+  }, [theme])
+
+  const toggleTheme = () => {
+    if(theme === 'light-theme'){
+      setTheme('dark-theme')
+    }else{
+      setTheme('light-theme')
+    }
+  }
   return <main>
     <nav>
       <div className="nav-center">
         <h1>overeacted</h1>
-        <button className="btn">
+        <button className="btn" onClick={toggleTheme}>
           toggle
         </button>
       </div>
