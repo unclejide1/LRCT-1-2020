bbca0471647f1cdaee4b2f862177a60e9ad0056c
Jideofor <njusticej@gmail.com>
2021-02-15 22:26:25 +0100

    feat: fetch questions from api

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -26,6 +26,29 @@ const AppProvider = ({ children }) => {
 
   const [isModalOpen, SetIsModalOpen] = useState(false)
 
+  const fetchQuestions = async (url) => {
+    setIsLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch(err => console.log(err))
+    if(response){
+     const data = response.data.results
+     if(data.length > 0){
+       setQuestions(data)
+       setIsLoading(false)
+       setWaiting(false)
+       setError(false)
+     }else{
+       setWaiting(true)
+       setError(true)
+     }
+    }else{
+      setWaiting(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
   return <AppContext.Provider value={{waiting,isLoading,questions,index,correct,
     error,isModalOpen}}>{children}</AppContext.Provider>
 }
