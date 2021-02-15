f1bdb828c1afb8684ab545484a59db7b036313db
Jideofor <njusticej@gmail.com>
2021-02-15 02:19:00 +0100

    feat: add funtionality to remove story from UI

--- hacker-news/src/context.js
+++ hacker-news/src/context.js
@@ -31,16 +31,22 @@ const AppProvider = ({ children }) => {
       const response = await fetch(url)
       const data = await response.json()
       dispatch({type: SET_STORIES, payload: {hits: data.hits, nbPages:data.nbPages}})
-      console.log(data);
     } catch (error) {
       console.log(error);
     }
   }
 
+  const removeStory = (id) => {
+    dispatch({
+      type: REMOVE_STORY,
+      payload: id,
+    })
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
   }, [])
-  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
+  return <AppContext.Provider value={{...state, removeStory}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
