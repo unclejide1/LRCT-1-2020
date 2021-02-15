2cc9c4f2923b3034f9a9bde8bc228e7d24e112af
Jideofor <njusticej@gmail.com>
2021-02-15 02:46:03 +0100

    feat: add change page functionality

--- hacker-news/src/context.js
+++ hacker-news/src/context.js
@@ -50,10 +50,17 @@ const AppProvider = ({ children }) => {
     })
   }
 
+  const handlePage = (value) => {
+    dispatch({
+      type: HANDLE_PAGE,
+      payload: value,
+    })
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [state.query])
-  return <AppContext.Provider value={{...state, removeStory, handleSearch}}>{children}</AppContext.Provider>
+  }, [state.query, state.page])
+  return <AppContext.Provider value={{...state, removeStory, handleSearch, handlePage}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
