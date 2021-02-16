82b78fcd0ae016d09fc20c177ad9d6975680d067
Jideofor <njusticej@gmail.com>
2021-02-16 17:24:51 +0100

    feat: randomize position of correct answer

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -5,7 +5,7 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const {waiting, isLoading, questions, index, correct, nextQuestion, checkAnswer, closeModal, isModalOpen} = useGlobalContext()
+  const {waiting, isLoading, questions, index, correct, nextQuestion, checkAnswer, isModalOpen} = useGlobalContext()
 
 
   if(waiting){
@@ -16,7 +16,15 @@ function App() {
     return <Loading/>
   }
   const {question, incorrect_answers, correct_answer} = questions[index]
-  const answers = [...incorrect_answers, correct_answer]
+  const answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random() * 4)
+  if(tempIndex === 3){
+    answers.push(correct_answer)
+  }else{
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
+  console.log(tempIndex);
   return <main>
     {isModalOpen && <Modal/>}
   
