bf2571797ca6bebe24380ac90d102a5b459a7a80
Jideofor <njusticej@gmail.com>
2021-02-16 11:31:10 +0100

    feat: display questions

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -5,7 +5,37 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+  const {waiting, isLoading, questions, index, correct} = useGlobalContext()
+
+
+  if(waiting){
+    return <SetupForm/>
+  }
+
+  if(isLoading){
+    return <Loading/>
+  }
+  const {question, incorrect_answers, correct_answer} = questions[index]
+  const answers = [...incorrect_answers, correct_answer]
+  return <main>
+    {/* <Modal/> */}
+    <section className="quiz">
+      <p className="correct-answers">
+        correct answers : {correct}/{index}
+      </p>
+      <article className="container">
+        <h2 dangerouslySetInnerHTML={{__html: question}}/>
+        <div className="btn-container">
+          {answers.map((answer, index) => {
+            return <button key={index} className="answer-btn" dangerouslySetInnerHTML={{__html:answer}}/>
+          })}
+        </div>
+      </article>
+      <button className="next-question">
+        next question
+      </button>
+    </section>
+    </main>
 }
 
 export default App
